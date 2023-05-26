61636f7e6f4eb467095d4107e7ab307e5ae51296
Shaf8808 <shadmankhan08@gmail.com>
2023-05-26 10:12:28 +0100

    Update run.py

--- run.py
+++ run.py
@@ -1,11 +1,13 @@
 import os
 from flask import Flask
 
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def index():
-    return "Hello, World"
+    return "Hello World!!!!"
 
 
 if __name__ == "__main__":

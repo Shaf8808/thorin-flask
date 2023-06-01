86444ac3ab6421b6ec6413cb17d8b1c7fcad2270
Shaf8808 <shadmankhan08@gmail.com>
2023-06-01 11:41:57 +0000

    Start on app.route function for member names url

--- run.py
+++ run.py
@@ -19,6 +19,12 @@ def about():
     return render_template("about.html", page_title="About", company=data)
 
 
+@app.route("/about/<member_name>")
+def about_member(member_name):
+    member = {}
+    with open()
+
+
 @app.route("/contact")
 def contact():
     return render_template("contact.html", page_title="Contact")
